6bb4d7e1329c82d69b8c522669e0c63c688c3a97
eunjun123 <113625608+eunjun123@users.noreply.github.com>
2022-12-16 18:40:01 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using static System.Console;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ACodeInput = char.Parse(Console.ReadLine());
+            int ascii = Convert.ToInt32(ACodeInput);
+            Console.WriteLine(ascii);
+        }
+    }
